bb4919abb7f09e26ee733c5dd7696b5c55fd67a4
ryanisnan <ryanisnan@gmail.com>
2017-08-08 22:35:25 -0700

    make the engine into a lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,15 @@
+extern crate ggez;
+
+mod assets;
+mod entity;
+mod camera;
+mod world;
+
+mod engine {
+
+}
+
+
 #[cfg(test)]
 mod tests {
     #[test]
--- src/world.rs
+++ src/world.rs
@@ -1,4 +1,3 @@
-extern crate rand;
 extern crate ggez;
 
 use assets::AssetLoader;
