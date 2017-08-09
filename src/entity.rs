ee6d80b1f5bae2aa8e4f6bd2dafe5c03d4947aa1
ryanisnan <ryanisnan@gmail.com>
2017-08-09 16:07:36 -0700

    add the concept of an entity library

--- src/entity.rs
+++ src/entity.rs
@@ -1,5 +1,8 @@
 use ggez::graphics::Image;
+use std::any::Any;
 use std::collections::HashMap;
+use std::collections::hash_map::Entry;
+use std::hash::Hash;
 use std::rc::Rc;
 
 pub trait Entity {} // Things that can be placed into the world will all implement this functionality
@@ -8,6 +11,11 @@ pub trait Entity {} // Things that can be placed into the world will all impleme
 #[derive(Debug)]
 pub struct DecorationType {
     pub image: Rc<Image>
+}
+
+#[derive(Debug)]
+pub struct Decoration {
+    pub meta: Rc<DecorationType>,
 }
 
 #[derive(Debug)]
@@ -27,7 +35,31 @@ impl DecorationLibrary {
     }
 }
 
-#[derive(Debug)]
-pub struct Decoration {
-    pub meta: Rc<DecorationType>,
+
+
+
+
+
+pub struct EntityLibrary {
+    /*
+        Holds a hash of types of entities and the various entities corresponding
+        to each type.
+    */
+    entities: HashMap<String, Box<Any>>,
+}
+
+impl EntityLibrary {
+    pub fn new() -> Self {
+        EntityLibrary {
+            entities: HashMap::new()
+        }
+    }
+
+    pub fn insert<T: Entity + 'static>(&mut self, entity_id: &str, entity: T) -> () {
+        self.entities.insert(entity_id.into(), Box::new(Rc::new(entity)));
+    }
+
+    pub fn get<T: Entity + 'static>(&self, entity_id: &str) -> Rc<T> {
+        self.entities[entity_id].downcast_ref::<Rc<T>>().unwrap().clone()
+    }
 }
