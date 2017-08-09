31dcff6ef29a10a162b2351976b4d84b5aaa38e4
ryanisnan <ryanisnan@gmail.com>
2017-08-09 16:36:31 -0700

    implement Entity trait for decoration

--- src/decoration.rs
+++ src/decoration.rs
@@ -1,10 +1,14 @@
 use ggez::graphics::Image;
 use std::rc::Rc;
 
+use entity::Entity;
+
 #[derive(Debug)]
 pub struct DecorationPrototype {
     pub image: Rc<Image>
 }
+
+impl Entity for DecorationPrototype {}
 
 #[derive(Debug)]
 pub struct Decoration {
