722df6ec74922b04c17b5afb214f447669993f04
ryanisnan <ryanisnan@gmail.com>
2017-08-09 16:46:05 -0700

    re-add DecorationLibrary - I don't like the other approach

--- src/decoration.rs
+++ src/decoration.rs
@@ -1,16 +1,31 @@
 use ggez::graphics::Image;
+use std::collections::HashMap;
 use std::rc::Rc;
-
-use entity::Entity;
 
 #[derive(Debug)]
 pub struct DecorationPrototype {
     pub image: Rc<Image>
 }
 
-impl Entity for DecorationPrototype {}
-
 #[derive(Debug)]
 pub struct Decoration {
     pub meta: Rc<DecorationPrototype>,
+}
+
+#[derive(Debug)]
+pub struct DecorationLibrary {
+    // Represents a library of different decoration types
+    pub decorations: HashMap<String, Rc<DecorationPrototype>>
+}
+
+impl DecorationLibrary {
+    pub fn new() -> Self {
+        DecorationLibrary {
+            decorations: HashMap::new(),
+        }
+    }
+
+    pub fn load(&mut self, decoration_id: &str, decoration_type: DecorationPrototype) {
+        self.decorations.insert(String::from(decoration_id), Rc::new(decoration_type));
+    }
 }
